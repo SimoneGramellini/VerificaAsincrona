fa9e70e6832edd937c5ce6341ae8563abe734ed7
simone.gramellini <simone.gramellini@PC7310>
2021-04-21 10:31:01 +0200

    Aggiunta logica estrazione lettera e componimento parola

--- VerificaAsincronoGramellini/VerificaAsincronoGramellini/MainWindow.xaml.cs
+++ VerificaAsincronoGramellini/VerificaAsincronoGramellini/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace VerificaAsincronoGramellini
 {
@@ -20,9 +21,53 @@ namespace VerificaAsincronoGramellini
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random r;
+        List<Char> lettere;
+        string parolaEstratta;
         public MainWindow()
         {
             InitializeComponent();
+            r = new Random();
+            AggiungiLettereALista();
+            EstraiLettera();
+        }
+
+        public async void EstraiLettera()
+        {
+            await Task.Run(() =>
+            {
+                char ultimaLetteraEstratta;
+                while (true)
+                {
+                    Thread.Sleep(300);
+                    ultimaLetteraEstratta = lettere[r.Next(0, 10)];
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        lblLettera.Content = ultimaLetteraEstratta;
+                    }));
+                }
+
+            });
+        }
+        public void AggiungiLettereALista()
+        {
+            lettere = new List<char>();
+            lettere.Add('A');
+            lettere.Add('G');
+            lettere.Add('H');
+            lettere.Add('S');
+            lettere.Add('J');
+            lettere.Add('X');
+            lettere.Add('E');
+            lettere.Add('T');
+            lettere.Add('O');
+            lettere.Add('I');
+        }
+
+        private void btnEstrai_Click(object sender, RoutedEventArgs e)
+        {
+            parolaEstratta += lblLettera.Content;
+            lblParolaEstratta.Content = parolaEstratta;
         }
     }
 }
